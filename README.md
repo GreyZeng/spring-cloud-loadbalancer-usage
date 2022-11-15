a6b0ca3d6c2b3cc9b14491056b8d968cab8916ba
GreyZeng <410486047@qq.com>
2022-11-15 12:55:36 +0800

    update code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # spring-cloud-loadbalancer-usage
+
+notes：
+
+启动 server 的时候，需要启动多实例，且每个实例要定义 VM options
+
+实例1： 
+
+定义 VM options 为 `-Dserver.port=8090`
+
+实例2：
+
+定义 VM options 为 `-Dserver.port=9092`
+
+实例3：
+
+定义 VM options 为 `-Dserver.port=9999`
--- client/src/main/java/git/snippet/client/config/WebClientConfig.java
+++ client/src/main/java/git/snippet/client/config/WebClientConfig.java
@@ -1,14 +1,21 @@
 package git.snippet.client.config;
 
-import git.snippet.client.config.SayHelloConfiguration;
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-@LoadBalancerClient(name = "server", configuration = SayHelloConfiguration.class)
+@LoadBalancerClient(name = "server")
 public class WebClientConfig {
 
     @LoadBalanced
@@ -17,4 +24,24 @@ public class WebClientConfig {
         return WebClient.builder();
     }
 
+    @Bean
+    @Primary
+    ServiceInstanceListSupplier serviceInstanceListSupplier() {
+        return new ServiceInstanceListSupplier() {
+            @Override
+            public String getServiceId() {
+                return "server";
+            }
+
+            @Override
+            public Flux<List<ServiceInstance>> get() {
+                return Flux.just(Arrays.asList(
+                        new DefaultServiceInstance(getServiceId() + "1", getServiceId(), "localhost", 8090, false),
+
+                        new DefaultServiceInstance(getServiceId() + "2", getServiceId(), "localhost", 9092, false),
+
+                        new DefaultServiceInstance(getServiceId() + "3", getServiceId(), "localhost", 9999, false)));
+            }
+        };
+    }
 }
